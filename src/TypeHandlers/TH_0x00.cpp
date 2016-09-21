bc59fa0f3188e25693977ac15cacb708f74213d0
Adrien Bertrand <bertrand.adrien@gmail.com>
2016-09-21 21:25:03 +0200

    Cleanup

--- src/TypeHandlers/TH_0x00.cpp
+++ src/TypeHandlers/TH_0x00.cpp
@@ -7,7 +7,6 @@
 
 #include "TypeHandlers.h"
 #include "../utils.h"
-#include <regex>
 #include <cfloat>
 
 // TODO : check if the models have different exponent offsets
--- src/TypeHandlers/TypeHandlers.h
+++ src/TypeHandlers/TypeHandlers.h
@@ -19,10 +19,7 @@ namespace tivars
     { th(); }
 
     namespace TH_0x00   // Real
-    {
-        th();
-        const constexpr size_t dataByteCount = 9;
-    }
+    { th(); }
 
     namespace TH_0x01   // Real list
     { th(); }
@@ -31,11 +28,7 @@ namespace tivars
     { th(); }
 
     namespace TH_0x05   // Program
-    {
-        th();
-        std::string reindentCodeString(const std::string& str_orig);
-        void initTokens();
-    }
+    { th(); }
 
     /* The following ones use the same handlers as 0x05 */
     namespace TH_0x03 = TH_0x05; // Y-Variable
@@ -46,6 +39,21 @@ namespace tivars
     { th(); }
 
 #undef th
+
+
+    /* Additional things */
+
+    namespace TH_0x00
+    {
+        const constexpr size_t dataByteCount = 9;
+        const std::string validPattern = "([-+]?[0-9]*\\.?[0-9]+(?:[eE][-+]?[0-9]{1,2})?)";
+    }
+
+    namespace TH_0x05   // Program
+    {
+        std::string reindentCodeString(const std::string& str_orig);
+        void initTokens();
+    }
 
 
 typedef decltype(&DummyHandler::makeDataFromString) dataFromString_handler_t;
