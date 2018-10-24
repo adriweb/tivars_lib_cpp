6db0dd95a9bac863caffed456f9e9f50639b97d5
Adrien Bertrand <bertrand.adrien@gmail.com>
2018-10-25 01:12:12 +0800

    TH_TempEqu: don't ignore the size field

--- src/TypeHandlers/TH_TempEqu.cpp
+++ src/TypeHandlers/TH_TempEqu.cpp
@@ -22,26 +22,33 @@ namespace tivars
     {
         (void)options;
 
-        size_t dataSize = data.size();
+        const ssize_t writtenSize = (data[0] & 0xFF) + ((data[1] & 0xFF) << 8);
+        const ssize_t dataSize    = data.size() - 2;
+
         if (dataSize < 6)
         {
             throw std::invalid_argument("Invalid data array. Needs to contain at least 6 bytes (size fields)");
         }
 
-        uint8_t type = data[0];
-        std::string typeStr = (type == 5 ? "prgm" : "");
+        if (writtenSize != dataSize)
+        {
+            throw std::invalid_argument("Invalid data array. Length field inconsistent with actual data");
+        }
 
-        uint8_t namelen = data[1];
+        uint8_t type = data[2];
+        std::string typeStr = ((type == 5 || type == 6) ? "prgm" : "");
+
+        uint8_t namelen = data[3];
         if (namelen < 1 || namelen > 8 || namelen > dataSize-4) { // for the other fields
             throw std::invalid_argument("Invalid data array. namelen field impossible or inconsistent with actual data");
         }
 
         std::string name(namelen, '\0');
-        memcpy(&name[0], &data[2], namelen);
+        memcpy(&name[0], &data[4], namelen);
 
-        uint16_t offset = (uint16_t) ((data[2 + namelen] & 0xFF) + ((data[2 + namelen + 1] & 0xFF) << 8));
+        uint16_t offset = (uint16_t) ((data[4 + namelen] & 0xFF) + ((data[4 + namelen + 1] & 0xFF) << 8));
 
-        data_t tokens(data.begin()+4+namelen-2, data.end()); // we take 2 bytes more at the beginning to overwrite them with length
+        data_t tokens(data.begin()+6+namelen-2, data.end()); // we take 2 bytes more at the beginning to overwrite them with length
         tokens[0] = (uchar) ((tokens.size()-2) & 0xFF);
         tokens[1] = (uchar) (((tokens.size()-2) >> 8) & 0xFF);
         std::string detokenized = TH_Tokenized::makeStringFromData(tokens);
